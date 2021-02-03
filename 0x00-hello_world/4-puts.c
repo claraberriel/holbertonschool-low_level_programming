7565663809ee6e925cefea466b7647e4c0179e3e
claraberriel <berrielclara@gmail.com>
2021-02-03 16:42:03 +0000

    first program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* A programs that prints exactly "Programming is like building a multilingual puzzle
+followed by a new line \n  - the puts functions adds it
+using puts function 
+escape secial char " by \"  */
+
+int main ()
+{
+  puts ("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
