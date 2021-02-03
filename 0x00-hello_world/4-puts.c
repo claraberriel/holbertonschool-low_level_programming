fd15c4d4d4eddc368a1cda1386a4bcb65d77bc82
claraberriel <berrielclara@gmail.com>
2021-02-03 17:20:40 +0000

    i hate betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-
-/* A programs that prints exactly "Programming is like building a multilingual puzzle
-followed by a new line \n  - the puts functions adds it
-using puts function 
-escape secial char " by \"  */
-
-int main ()
+int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
