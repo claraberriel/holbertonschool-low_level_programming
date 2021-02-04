1ebb328a0595287667a6085cc3e8fa5cdf26ade8
claraberriel <berrielclara@gmail.com>
2021-02-04 16:11:16 +0000

    compiler error

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* Return: 0 if correct
+*/
+
+int main(void)
+{
+int n;
+int l;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+l = n % 10;
+if (l > 5)
+{
+printf("Last string of %i", "is %i and is greater than 5\n", n, l);
+}
+if (l == 0)
+{
+printf("Last string of %i", "is %i and is 0\n", n, l);
+}
+if (l < 6 && l != 0)
+{
+printf("Last digit of %i", "is %i and is less than 6 and not 0\n", n, l);
+}
+return (0);
+}
