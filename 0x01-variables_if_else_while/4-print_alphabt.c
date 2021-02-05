d011d1f7f2b7a6d2e6b63f1dcbb264d32a88e845
claraberriel <berrielclara@gmail.com>
2021-02-05 00:55:29 +0000

    q e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: 0 if correct
+*/
+
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c == 'q' || c == 'e')
+continue;
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
