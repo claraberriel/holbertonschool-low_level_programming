5ede1ca11c5044bd8030419b59b5c7228b5695f7
claraberriel <berrielclara@gmail.com>
2021-02-05 01:32:12 +0000

    ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: 0 if correct
+*/
+
+int main(void)
+{
+int num = 48;
+while (num < 58)
+{
+putchar(num);
+num++;
+}
+putchar(10);
+return (0);
+}
