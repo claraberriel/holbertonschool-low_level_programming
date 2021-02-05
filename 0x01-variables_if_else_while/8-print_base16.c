bcf51e230ce451f2be7a1181f118a64ed23f6718
claraberriel <berrielclara@gmail.com>
2021-02-05 01:56:08 +0000

    h

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main -Entry Point
+* Return: 0 if correct
+*/
+
+int main(void)
+{
+for (int v = 48; v < 58; v++)
+{
+putchar (v);
+}
+for (int f = 97; f < 103; f++)
+{
+putchar (f);
+}
+putchar (10);
+return (0);
+}
