096d6d569bfcb1e042c55f818fae08198636ae46
claraberriel <berrielclara@gmail.com>
2021-02-05 12:39:41 +0000

    done

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: 0 if correct
+*/
+
+int main(void)
+{
+
+int i = 0;
+
+while (i <= 99)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+
+if (i != 99)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+
+putchar('\n');
+return (0);
+}
