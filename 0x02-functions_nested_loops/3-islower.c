995ad6eb53d8316da055f23ee2a9a6077727a68e
claraberriel <berrielclara@gmail.com>
2021-02-09 13:43:19 +0000

    damn thouse betty comments

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 
-/*
-* _islower - check for lowercase
+/**
+* _islower - Check for lowercase.
+* @c: input value
+*
+* Description: check lowercase using ASCII
+* section header: holberton
 * Return: 1 if lower, else 0
 */
 
 int _islower(int c)
 {
-
 if (c < 123 && c > 96)
 {
 return (1);
 }
 else
-{
 return (0);
-}
 }
