defd3d9beceb857c4a6ae67127fb3296ba645fc9
claraberriel <berrielclara@gmail.com>
2021-02-09 01:41:34 +0000

    wrong

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/*
+ * _isalpha - compiler is not gonna like this description anyway
+ * Return: 1 if letter, else 0
+ */
+
+int _isalpha(int c)
+{
+if (c > 64 && c < 123)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/*
+ * print_sign - prints sign
+ * Return: 1 if > 0, 0 if = 0, -1 if < 0
+ */
+
+int print_sign(int n)
+char x
+{
+
+if (n > 0)
+{
+return (1);
+_putchar('+');
+}
+
+else if (n == 0)
+{
+return (0);
+_putchar('0');
+}
+
+else
+{
+return (-1);
+_putchar('-');
+}
+
+}
