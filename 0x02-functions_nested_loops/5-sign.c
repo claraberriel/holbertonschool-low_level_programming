8606cd3996089cca090dc4d7773bd921c13f73f6
claraberriel <berrielclara@gmail.com>
2021-02-09 13:55:08 +0000

    function body return goes last

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,30 @@
 #include "holberton.h"
 
-/*
+/**
  * print_sign - prints sign
+ * @n: variable
  * Return: 1 if > 0, 0 if = 0, -1 if < 0
  */
 
 int print_sign(int n)
-char x
 {
 
 if (n > 0)
 {
-return (1);
 _putchar('+');
+return (1);
 }
 
 else if (n == 0)
 {
-return (0);
 _putchar('0');
+return (0);
 }
 
 else
 {
-return (-1);
 _putchar('-');
+return (-1);
 }
 
 }
