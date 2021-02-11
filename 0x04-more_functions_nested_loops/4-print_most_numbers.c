7225280c10cf16e0edc3c8ede9f7039cc2393fb5
claraberriel <berrielclara@gmail.com>
2021-02-11 14:03:51 +0000

    think it backwards

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Print numbers form 0 to 9 except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+int c;
+for (c = 48;  c < 58; c++)
+{
+
+if (c != 50 && c != 52)
+_putchar(c);
+
+}
+_putchar('\n');
+}
