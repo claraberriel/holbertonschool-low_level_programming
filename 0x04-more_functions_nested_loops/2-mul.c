fff8a722f2d4f3ec07d0c0ab9a0a9f606e7a930e
claraberriel <berrielclara@gmail.com>
2021-02-11 13:14:21 +0000

    *

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiplies 2 ints
+ * @a: inherited from main file
+ * @b: inherited from main file
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+int c = a * b;
+return (c);
+}
