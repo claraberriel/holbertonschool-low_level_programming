58351d0c9e4dc5862279e0df1ac3d7305e373336
claraberriel <berrielclara@gmail.com>
2021-02-11 13:23:34 +0000

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints numbers 0 to 9 \n
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
