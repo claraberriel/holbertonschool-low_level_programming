75c6d90ff1f27be42166b28d45a65756330ff016
claraberriel <berrielclara@gmail.com>
2021-02-16 11:08:58 -0300

    #betty

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/*
+* swap_int(int *a, int *b) - Swaps values
+* @a: inherited from main
+* @b: ""
+*/
+
+void swap_int(int *a, int *b);
+{
+    int temp = *a;
+
+    *a = *b;
+    *b = temp;
+}
