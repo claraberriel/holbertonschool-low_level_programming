f4cecbc00d1a69ae7edbd8e29fea75bd368bf915
claraberriel <berrielclara@gmail.com>
2021-02-16 11:57:04 -0300

    str

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,19 @@
+#include <stdio.h>
 #include "holberton.h"
 
 /**
- * int _strlen
- * 
+ * _strlen - length of a string
+ * @s: pointer to string
+ * Return: length of string
  */
 
 int _strlen(char *s)
+{
+int str = 0;
+
+while (s[str] != '\0')
+{
+str++;
+}
+return (str);
+}
