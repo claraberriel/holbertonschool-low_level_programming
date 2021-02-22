51888963c86e2da0141333a69d13d99969f76095
claraberriel <berrielclara@gmail.com>
2021-02-22 18:12:16 -0300

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -28,7 +28,7 @@
 char *_strcat(char *dest, char *src)
 {
 int count = 0, j;
-/**
+/*
  * stores the length of the destination string on the count variable
 */
 while (dest[count] != '\0')
@@ -36,7 +36,7 @@ while (dest[count] != '\0')
 ++count;
 }
 
-/**
+/*
  * concatenates source string to destination string
 */
 for (j = 0; src[j] != '\0'; ++j, ++count)
@@ -44,7 +44,7 @@ for (j = 0; src[j] != '\0'; ++j, ++count)
 dest[count] = src[j];
 }
 
-/**
+/*
  * terminates the destination string
 */
 dest[count] = '\0';
