454c6ae3bb1d2f587dcdd31ff55b9df2b9b9658e
claraberriel <berrielclara@gmail.com>
2021-02-22 18:25:51 -0300

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,3 @@
-/*
-* Write a function that concatenates two strings
-* This function appends the src string to the dest string, overwriting the
-* terminating null byte (\0) at the end of dest, and then adds
-* a terminating null byte
-* Returns a pointer to the resulting string dest
-*
-*gcc -Wall -pedantic -Werror -Wextra 0-main.c 0-strcat.c -o 0-strcat
-* Hello
-* World!
-* Hello World!
-* World!
-* Hello World!
-* Output:
-*
-*
-*/
-
 #include "holberton.h"
 
 /**
