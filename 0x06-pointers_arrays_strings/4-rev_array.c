5739b7586b86b9c5ac2419894deeaddb7db72f89
claraberriel <berrielclara@gmail.com>
2021-02-22 20:43:56 -0300

    .

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,19 +1,32 @@
-/** Write a function that reverses the content of an array of integers.  
- * Where n is the number of elements of the array
- * 
- * gcc -Wall -pedantic -Werror -Wextra 4-main.c 4-rev_array.c -o 4-rev_array
- * 
- * Output:
- * 
- * 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337
- * 1337, 1024, 98, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
- */
-
 #include "holberton.h"
 
 /**
- * 
- * 
+ * reverse_array - reverses an array
+ * @a: pointer to array
  */
 
 void reverse_array(int *a, int n)
+{
+int count = 0, end, begin;
+
+/*
+* calculate string lenght
+*/
+while (a[count] != '\0')
+{
+count++;
+}
+
+end = count - 1;
+
+/*
+* reverse string
+*/
+for (begin = 0; begin < count; begin++)
+{
+n[begin] = a[end];
+end--;
+}
+
+return (); //not 0 
+}
