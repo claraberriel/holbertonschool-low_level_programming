6d02e9a3cc4a1674c9e4d194c6d970a191523978
claraberriel <berrielclara@gmail.com>
2021-02-23 16:57:59 -0300

    .

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,13 +9,15 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int r, c;
+	int x, y;
+	char aux;
 
-	for (r = 0; r < 8; r++)
+	for (x = 0; x < 8; x++)
 	{
-		for (c = 0; c < 8; c++)
+		for (y = 0; y < 8; y++)
 		{
-			_putchar(a[r][c]);
+			aux = a[x][y];
+			_putchar(aux);
 		}
 		_putchar('\n');
 	}
