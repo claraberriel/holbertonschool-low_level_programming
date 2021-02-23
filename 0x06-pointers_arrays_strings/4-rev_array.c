842adbbbb70de2c43809eba8b1f015511fbccc99
claraberriel <berrielclara@gmail.com>
2021-02-22 21:07:20 -0300

    .

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,21 +2,20 @@
 
 /**
  * reverse_array - reverses an array
- * @a: pointer to array
+ * @a: array of int
+ * @n: the number of elements to swap
  */
 
 void reverse_array(int *a, int n)
 {
-int temp = 0, end = n - 1, begin;
+int end = n - 1, begin, temp; 
 
-/*
-* reverse string
-*/
-for (begin = 0; begin < temp; begin++)
+for (begin = 0; begin < end; begin++)
 {
 temp = a[end];
 a[end] = a[begin];
 a[begin] = temp;
 end--;
 }
+
 }
