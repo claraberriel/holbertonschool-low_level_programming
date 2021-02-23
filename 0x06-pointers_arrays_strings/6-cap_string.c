ae6b0384e6283ff23d9292c573d1d5808c5309ae
claraberriel <berrielclara@gmail.com>
2021-02-22 21:47:13 -0300

    .

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,4 +18,26 @@
  * 
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
+{
+int i, prev= str[i] -1;
+
+/*
+* iterates the sting
+*/
+while (str[i]) != '\0') 
+{
+i++;
+
+/*
+* check previous char and ASCII values for lowercase chars
+*/
+while (prev == ' ' || prev == '\t' || prev == '\0' || prev == ',' | prev == ';' | prev == '.' | prev == '!' | prev == '?' | prev == '"' | prev == '(' | prev == ')' | prev == '{' | prev == '}')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] -= 32;
+}
+
+}
+return (str);
+}
