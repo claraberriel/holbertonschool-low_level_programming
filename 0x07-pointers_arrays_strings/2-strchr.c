03fbf722b3170d12b7d91f2f48ca919def1fee20
claraberriel <berrielclara@gmail.com>
2021-02-23 14:25:10 -0300

    wonderwall

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 
 /**
- * 
- * 
+ * _strchr - function that locates a character in a string.
+ * @s: string
+ * @c: char
+ * Return: pointer to 1st ocurrance fo c, else null
  */
 
 char *_strchr(char *s, char c)
