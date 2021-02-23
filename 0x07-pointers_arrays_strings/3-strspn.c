6796e256c91cdf31172c2c546a5ea59b91daf2f9
claraberriel <berrielclara@gmail.com>
2021-02-23 16:40:59 -0300

    return def

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,8 @@
  * _strchr - function that gets the length of a prefix substring.
  * @s: string
  * @accept: number of bytes
- * Return: 
+ * Return: the number of bytes in the initial segment of s 
+ * which consist only of bytes from accept
  */
 
 unsigned int _strspn(char *s, char *accept)
