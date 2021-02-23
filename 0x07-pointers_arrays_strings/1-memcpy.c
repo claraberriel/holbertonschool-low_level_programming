3c3e6d49a7491f11df7f433ea577537cddcfab34
claraberriel <berrielclara@gmail.com>
2021-02-23 13:56:50 -0300

    fff

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,18 +10,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
- /*
- * Typecast src and dest addresses to (char *)
- */
-char *csrc = (char *)src;
-char *cdest = (char *)dest;
-
-/*
-* Copy contents of src[] to dest[]
-*/
 unsigned int i;
-for (int i = 0; i < n; i++)
-cdest[i] = csrc[i];
 
-return (cdest);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
