1db719be6f8f96f1c374e656225255507cedc71e
claraberriel <berrielclara@gmail.com>
2021-02-22 21:20:42 -0300

    .

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,44 @@
-/** Write a function that changes all lowercase 
- * letters of a string to uppercase.
- * 
- * gcc -Wall -pedantic -Werror -Wextra 5-main.c 5-string_toupper.c -o 5-string_toupper
- * 
- * Output:
- * 
- * HOLBERTON SCHOOL!
- * HOLBERTON SCHOOL!
- */
+/*
+* Write a function that changes all lowercase to upper
+* The value of a in ASCII is 97, 
+* if we subtract 32 it becomes 65 which is ASCII value of A.
+* All lowercase characters ASCII value is from 97 to 122 
+* and if we subtract 32 in each lowercase character 
+* only then it will become uppercase character
+*/
 
 #include "holberton.h"
 
 /**
- * 
- * 
+ * string_toupper - changes lowercase chars to upper
+ * Return: 
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *c)
+{
+int i = 0;
+
+/*
+* iterate loop until end of string
+*/
+while (c[i] != '\0')
+{
+
+/*
+* If char is lowercase then substarct 32
+*(because of ASCII value explained above)
+*/
+if (c[i] >= 'a' && c[i] <= 'z')
+{
+c[i] -= 32;
+}
+
+/*
+increase iterator variable
+*/
+i++;
+}
+
+return (c);
+
+}
