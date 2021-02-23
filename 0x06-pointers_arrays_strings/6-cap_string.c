1a8b299f7ab418232dfee8a50a1045cd6ed4e2bd
claraberriel <berrielclara@gmail.com>
2021-02-22 21:49:15 -0300

    .

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,14 @@
-/** Write a function that capitalizes all words of a string
- * Separators of words: space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }
- * 
- * gcc -Wall -pedantic -Werror -Wextra 6-main.c 6-cap_string.c -o 6-cap
- * 
- * Output:
- * 
- * Expect The Best. Prepare For The Worst. Capitalize On What Comes.
- * Hello World! Hello-world 0123456hello World Hello World.Hello World
- * Expect The Best. Prepare For The Worst. Capitalize On What Comes.
- * Hello World! Hello-world 0123456hello World Hello World.Hello World
- */
-
 #include "holberton.h"
 
 /**
- * 
- * 
+ * cap_string - Capitalizes string
+ * @str: string
+ * * Return: str
  */
 
 char *cap_string(char *str)
 {
-int i, prev= str[i] -1;
+int i, prev = str[i] - 1;
 
 /*
 * iterates the sting
