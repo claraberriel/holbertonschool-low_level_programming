a260ab54872521ef3b4014b9646ec2b590ea7796
root <berrielclara@gmail.com>
2021-03-02 19:41:15 +0000

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns pointer to duplicated string
+ * @str: pointer to string
+ *
+ * Return: a pointer
+ */
+
+char *_strdup(char *str)
+{
+
+char *str2;
+char temp = str2;
+
+if (str == NULL)
+return (NULL);
+
+str = malloc((sizeof(char) * len) + 1)
+
+while (*str != '\0')
+{
+*temp = *str;
+++str;
+++temp;
+}
+*temp = '\0';
+return (*str2);
+}
+
+
+/**
+ * _strlen - length of a string
+ * @s: pointer to string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+int len = 0;
+
+while (s[len] != '\0')
+{
+len++;
+}
+return (len);
+}
