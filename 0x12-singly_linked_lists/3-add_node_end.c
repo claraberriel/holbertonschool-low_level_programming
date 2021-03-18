5256634bc985d94d10c4642d27a6735f457bcc64
claraberriel <berrielclara@gmail.com>
2021-03-18 14:32:24 -0300

    same as 2 but adding aux

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * _strlen - length of a string
+ * @s: pointer to string
+ * Return: length of string
+ */
+
+int _strlen(const char *s)
+{
+	int str = 0;
+
+	while (s[str] != '\0')
+	{
+	str++;
+	}
+	return (str);
+}
+
+/**
+ * add_node -  adds a new node at the beginning of a list
+ * @head: pointer to first element of the list
+ * @str: string
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *new, *aux;
+
+	if (head == NULL)
+		return (0);
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	if (new->str == NULL)
+		free(new);
+
+	new->len = _strlen(str);
+	aux = *head;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	
+	while (aux->next != NULL)
+	{
+		aux = aux->next;
+	}
+
+	new = aux->next;
+
+	return (new);
+}
