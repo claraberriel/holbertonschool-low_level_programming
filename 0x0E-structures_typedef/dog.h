9399fd226afbd825d026e06d242873b61392d30d
claraberriel <berrielclara@gmail.com>
2021-03-18 01:20:22 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
-#define dog
-#ifndef dog
+#ifndef DOG
+#define DOG
+
+/**
+* struct dog - check the code for Holberton School students.
+* @name: name of the owner
+* @owner: owner of the dog
+* @age: age of the dog
+* Return: Always 0.
+*/
 
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
