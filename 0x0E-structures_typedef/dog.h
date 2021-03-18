21a31a0347a26bd67c9d61dc05d502c41576921c
claraberriel <berrielclara@gmail.com>
2021-03-17 22:27:35 -0300

    protoype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,22 @@
+#define DOG
 #ifndef DOG
 #define DOG
 
 /**
-* struct dog - check the code for Holberton School students.
-* @name: name of the owner
-* @owner: owner of the dog
-* @age: age of the dog
-* Return: Always 0.
-*/
+ * struct dog - check the code for Holberton School students.
+ * @name: name of the owner
+ * @owner: owner of the dog
+ * @age: age of the dog
+ * Return: Always 0.
+ */
 
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
