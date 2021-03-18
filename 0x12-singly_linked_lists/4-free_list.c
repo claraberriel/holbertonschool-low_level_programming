2f0d8d0bda6806ea70d67706412fd72d9d78aa82
claraberriel <berrielclara@gmail.com>
2021-03-18 17:49:07 -0300

    susi

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - deallocates momery of list
+ * @head: pointer to first element of the linked list 
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *susi;
+
+	while (head != NULL)
+	{
+		susi = head;
+		head = head->next;
+		free(susi->str);
+		free(susi);
+	}
+	free(head);
+}
