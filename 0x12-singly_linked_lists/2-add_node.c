463d458269761b5ff26fff5641379f69d03f7e89
claraberriel <berrielclara@gmail.com>
2021-03-18 14:01:27 -0300

    malloc

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -28,4 +28,24 @@ int _strlen(char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 
+	list_t *new;
+
+	if (head == NULL)
+		return (0);
+	
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+			return (NULL);
+	
+	new->str = strdup(str);
+
+	if (new->str == NULL)
+		free(new);
+
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
