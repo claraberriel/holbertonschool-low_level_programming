d8b0333cac843bbd40e50f8084d012fcbe2e6f91
claraberriel <berrielclara@gmail.com>
2021-03-18 14:36:06 +0000

    fix indentation for betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,17 @@
 
 size_t print_list(const list_t *h)
 {
-        int i = 0;
+	int i = 0;
 
-        while (h != NULL)
-        {
-            if (h->str == NULL)
-            printf("[0] (nil)\n");
-            
-            else
-            printf("[%i] %s\n", h->len, h->str);
-
-            i += 1;
-            h = h->next;
-
-        }
-        return (i);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		i += 1;
+		h = h->next;
+	}
+	return (i);
 }
+
