c238e8bcba5351171971c76ec887452b4aa06707
claraberriel <berrielclara@gmail.com>
2021-03-22 19:57:15 -0300

    space prohibited after (

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (aux != NULL)
 	{
-		for ( nth = 0; nth < index; nth++)
+		for (nth = 0; nth < index; nth++)
 		{
 			aux = aux->next;
 		}
