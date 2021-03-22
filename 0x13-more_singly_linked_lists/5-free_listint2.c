bda18d29f42de70f49edc5fb04fb518a0a854904
claraberriel <berrielclara@gmail.com>
2021-03-22 18:23:50 -0300

    free mem and set head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - deallocates memory for a list and sets head to NULL
+ * @head: pointer to list
+ * Return: nth
+ */
+
+void free_listint2(listint_t **head)
+{
+	free_listint(*head);
+	*head = NULL;
+}
