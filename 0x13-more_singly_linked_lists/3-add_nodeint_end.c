1c50278fd5c6ce291086c583cda86a08c942168f
claraberriel <berrielclara@gmail.com>
2021-03-23 10:57:16 -0300

    missing return description

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,6 +4,7 @@
  * add_nodeint_end - adds new node at the end of the list
  * @head: pointer to begininng of list
  * @n: int value of new node
+ * Return: address of new element of NULL if fails
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
