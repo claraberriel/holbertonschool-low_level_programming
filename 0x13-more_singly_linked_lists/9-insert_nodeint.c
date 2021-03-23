b64f22cdce6d79f6e784eefe9b5d4ae2c551627c
claraberriel <berrielclara@gmail.com>
2021-03-23 17:25:30 -0300

    rtrn

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,9 +14,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int index = idx - 1;
 	listint_t *new, *aux;
 
+	idx = 0;
 	aux = *head;
 	if (head == NULL)
-		return (0);
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
