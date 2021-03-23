1d7c94e6f699f0ddcbed2c64c88fc04c685b13a7
claraberriel <berrielclara@gmail.com>
2021-03-23 11:33:49 -0300

    betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int nth;
-	unsigned int index = idx -1;
+	unsigned int index = idx - 1;
 	listint_t *new, *aux;
 
 	aux = *head;
