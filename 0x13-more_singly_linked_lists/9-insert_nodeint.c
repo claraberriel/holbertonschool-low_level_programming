26650e85642c9e19f6dd55e2d0409a85d2814637
claraberriel <berrielclara@gmail.com>
2021-03-23 11:29:49 -0300

    changing loop conditions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 
-	for (nth = 0; nth < index && aux != NULL; nth++)
+	while (aux != NULL && nth != index)
 	{
 		aux = aux->next;
 	}
