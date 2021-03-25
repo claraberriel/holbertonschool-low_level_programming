15dfb512f03a7946bfe47c08b111d584e089558f
claraberriel <berrielclara@gmail.com>
2021-03-25 20:39:10 -0300

    bin

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - onverts a binary number to an unsigned int
+ * @b: binary to convert
+ * Return: 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, n = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			n = n << 1;
+
+			if (b[i] == '1')
+				n += 1;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (n);
+}
