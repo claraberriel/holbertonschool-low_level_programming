945b0386d434123c0f14db0b349bda3ed8f4a21d
claraberriel <berrielclara@gmail.com>
2021-03-29 19:22:17 -0300

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,9 +5,9 @@
  * @filename: name of the file to read from
  * @letters: number of letters it should read and print
  * Return: actual number of letters it could read and print.
- * 			(A) if the file can not be opened or read, return 0
- * 			(B) if filename is NULL return 0
- *			(C) if write fails or does not write the expected amount of bytes, return 0
+ * (A) if the file can not be opened or read, return 0
+ * (B) if filename is NULL return 0
+ * (C) if write fails or does not write the expected amount of bytes, return 0
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -17,7 +17,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buf = malloc(letters * sizeof(char));
 
-	filedesc = open(filename, O_RDONLY); /* open(const char *path, int oflag, ...);	mode_t mode argument only required if oflag is O_CREAT */
+	/* open(const char *path, int oflag, ...); */
+	filedesc = open(filename, O_RDONLY);
 
 	/* Return condition (A) */
 	if (filedesc == -1)
@@ -27,10 +28,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	bytes_read = read(filedesc, buf, letters); /* read(int fildes, void *buf, size_t nbyte); */
+	/* read(int fildes, void *buf, size_t nbyte); */
+	bytes_read = read(filedesc, buf, letters);
 
-	bytes_written = write(STDOUT_FILENO, buf, bytes_read); /* write(int fildes, const void *buf, size_t nbyte); */
-
+	/* write(int fildes, const void *buf, size_t nbyte); */
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	
 	/* Return conditons (C) */
 	if (bytes_read != bytes_written || bytes_written == -1)
 		return (0);
