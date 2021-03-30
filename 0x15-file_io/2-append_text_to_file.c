b82b155f173b703ade2d219d75732f35f8f19e85
claraberriel <berrielclara@gmail.com>
2021-03-29 21:35:31 -0300

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,21 +1,22 @@
 #include "holberton.h"
 
 /**
- * create_file -  creates a file.
+ * append_text_to_file -  appends text at the end of a file.
  * @filename: is the name of the file to create
  * @text_content: is a NULL terminated string to write to the file
  * Return: 1 on success,
  * (A) -1 on failure: (file can not be created...
  * ...file can not be written, write “fails”, etc…)
  * (B) if filename is NULL return -1
+ *  Return 1 if the file exists and -1 if the file does not exist or
+ * if you do not have the required permissions to write the file
  *
  * Other requirements:
- * The created file must have those permissions: rw-------.
- * If the file already exists, do not change the permissions.
- * if the file already exists, truncate it
+ * Do not create the file if it does not exist
+ * If text_content is NULL, do not add anything to the file.
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int filedes, len;
 
@@ -23,8 +24,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/* open(const char *path, int oflag, mode_t mode) */
-	filedes = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	/* open(const char *path, int oflag, ...) */
+	filedes = open(filename, O_RDWR | O_APPEND);
 
 	if (text_content == NULL)
 	{
